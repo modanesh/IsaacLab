a7367f1469e1ae0e801d47d7ca59aa6c66b29478
modanesh <mohamad4danesh@gmail.com>
2026-01-07 11:15:58 -0500

    minor bug fixed

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/flexiv/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/flexiv/__init__.py
@@ -18,7 +18,7 @@ gym.register(
     id="Isaac-Lift-Cube-Flexiv-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.joint_pos_env_cfg: FlexivCubeLiftEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.joint_pos_env_cfg:FlexivCubeLiftEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LiftCubePPORunnerCfg",
     },
     disable_env_checker=True,
